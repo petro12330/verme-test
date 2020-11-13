b4528824214a1a6d184af923bdc6e14c1f4309da
petro12330 <52936784+petro12330@users.noreply.github.com>
2020-11-14 00:49:10 +0300

    Api test done

--- orgunits/api_v1/views.py
+++ orgunits/api_v1/views.py
@@ -21,4 +21,22 @@ class OrganizationViewSet(TokenAuthMixin, ModelViewSet):
         Возвращает родителей запрашиваемой организации
         TODO: Написать два действия для ViewSet (parents и children), используя методы модели
         """
-        return Response()
+
+        find=Organization.objects.get(name=self.get_object())
+        parents_list=Organization.parents(find)
+        serializer = self.get_serializer( parents_list, many=True)
+
+        return Response (serializer.data)
+
+    @action(methods=["GET"], detail=True)
+    def children(self, request, *args, **kwargs):
+        """
+        Возвращает родителей запрашиваемой организации
+        TODO: Написать два действия для ViewSet (parents и children), используя методы модели
+        """
+
+        find = Organization.objects.get(name=self.get_object())
+        children_list = Organization.children(find)
+        serializer = self.get_serializer(children_list, many=True)
+
+        return Response(serializer.data)
